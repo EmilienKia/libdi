36de11f13b34e079b10469f9ecb1f45bb06a39a7
Emilien KIA <emilien.kia@gmail.com>
2016-01-27 15:54:05 +0100

    Change license and add some info

--- tests/main.cpp
+++ tests/main.cpp
@@ -1,20 +1,21 @@
 /* -*- Mode: C; indent-tabs-mode: t; c-basic-offset: 4; tab-width: 4 -*-  */
 /*
- * main.cc
- * Copyright (C) 2016 Emilien Kia <emilien.kia@gmail.com>
+ * main.cpp
  * 
- * depinj is free software: you can redistribute it and/or modify it
- * under the terms of the GNU General Public License as published by the
- * Free Software Foundation, either version 3 of the License, or
+ * Copyright (C) 2016 Emilien Kia <emilien.kia@gmail.com>
+ *
+ * libdi is free software: you can redistribute it and/or modify it
+ * under the terms of the GNU Lesser General Public License as published
+ * by the Free Software Foundation, either version 3 of the License, or
  * (at your option) any later version.
  * 
- * depinj is distributed in the hope that it will be useful, but
+ * libdi is distributed in the hope that it will be useful, but
  * WITHOUT ANY WARRANTY; without even the implied warranty of
  * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.
- * See the GNU General Public License for more details.
+ * See the GNU Lesser General Public License for more details.
  * 
- * You should have received a copy of the GNU General Public License along
- * with this program.  If not, see <http://www.gnu.org/licenses/>.
+ * You should have received a copy of the GNU Lesser General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.";
  */
 
 #include <iostream>
